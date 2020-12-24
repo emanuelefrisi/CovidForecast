d2c34da75870354e1fca212bd85417d7ac508431
Emanuele Frisi <>
2020-12-24 11:56:40 +0100

    Creazione del progetto con Spring Inizializr. Aggiunte dipendenze Spring Boot DevTools, Spring Web, Spring Data JPA e MySQL Driver

---
+++ src/main/java/it/univpm/CovidForecast/CovidForecastApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.CovidForecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidForecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univpm/CovidForecast/CovidForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.CovidForecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
