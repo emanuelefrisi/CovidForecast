28c1e1f639eceef9852cc3810d77d82d1751a474
emanuelefrisi <s1092866@studenti.univpm.it>
2021-01-16 13:13:53 +0100

    Inizializzata la parte di front-end del progetto

---
+++ Front-end/src/it/univpm/CovidForecast/Main.java
@@ -0,0 +1,13 @@
+package it.univpm.CovidForecast;
+
+import it.univpm.CovidForecast.view.CFLogin;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		new CFLogin();
+		
+	}
+
+}
